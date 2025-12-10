4bfde2a3d1515555f3f99303893ab71418865003
Tania Lapa <taniamiroshnuchenko@gmail.com>
2025-12-10 13:58:15 +0100

    readyToCheck

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const registerForm = document.querySelector(".login-form");
+registerForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  
+  if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+
+
+const clickMe = document.querySelector(".change-color");
+const bodyBackground = document.querySelector("body");
+
+clickMe.addEventListener('click', handlerBtn)
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function handlerBtn(event) {
+  bodyBackground.style.backgroundColor = getRandomHexColor();
 }
